a91731007a9125b95185be92ce0c29530eba92b2
PieroMeza10 <pieroms0905@gmail.com>
2026-03-31 20:31:20 -0500

    EJER 1.2

--- app/src/main/java/com/PieroMeza/seguimiento_piero/CargaActivity.kt
+++ app/src/main/java/com/PieroMeza/seguimiento_piero/CargaActivity.kt
@@ -1,20 +1,56 @@
 package com.PieroMeza.seguimiento_piero
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class CargaActivity : AppCompatActivity() {
+
+    private lateinit var progressBar: ProgressBar
+    private lateinit var txtCargando: TextView
+    private val TAG = "CargaActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_carga)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        progressBar = findViewById(R.id.progressBarCarga)
+        txtCargando = findViewById(R.id.txtCargando)
+
+        val destino = intent.getStringExtra("destino")
+
+        Thread {
+            var progreso = 0
+
+            while (progreso <= 100) {
+                Thread.sleep(20)
+                progreso++
+
+                runOnUiThread {
+                    progressBar.progress = progreso
+                    txtCargando.text = "Cargando... $progreso%"
+                }
+            }
+
+            runOnUiThread {
+                txtCargando.text = "Carga completa"
+                Toast.makeText(this, "Carga completa", Toast.LENGTH_SHORT).show()
+                Log.d(TAG, "Carga completa")
+
+                val intentDestino = when (destino) {
+                    "segunda" -> Intent(this, SegundaActividad::class.java)
+                    "monedas" -> Intent(this, MonedasActivity::class.java)
+                    "drawer" -> Intent(this, DrawerActivity::class.java)
+                    else -> Intent(this, MainActivity::class.java)
+                }
+
+                startActivity(intentDestino)
+                finish()
+            }
+        }.start()
     }
 }
